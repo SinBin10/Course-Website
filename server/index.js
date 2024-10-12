7928f6061508f286f0b78a01117bc6b13c3e8f9d
SinBin10 <bincode10@gmail.com>
2024-10-12 19:31:37 +0530

    using express.json middleware to parse JSON data.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const router = require("./router/auth-router");
 
 const app = express();
 
+app.use(express.json());
+
 //As the number of routes increase in a project the main index.js file
 //becomes very cluttered so to make it clean and modular we make
 //handling of the response to another file called routers.
