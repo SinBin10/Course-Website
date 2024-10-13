f17bae338a37f17b326d6f2304240c519b8a8f65
SinBin10 <bincode10@gmail.com>
2024-10-13 11:55:30 +0530

    securing database credentials.

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
+const URI = process.env.MONGODB_URI;
+
 const connectDB = async () => {
   try {
-    await mongoose
-      .connect
-      //you can write the name of the database in between the forward slash and question mark
-      ();
+    //you can write the name of the database in between the forward slash and question mark    );
+    await mongoose.connect(URI);
     console.log("database connected successfully");
   } catch (error) {
     console.log("database connected failed", error);
