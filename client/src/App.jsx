076da9a7b4901b648f2aa58b482ed82966667a72
SinBin10 <bincode10@gmail.com>
2024-11-23 07:13:34 +0530

    installed tailwind css and started with navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,15 @@ import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Service from "./pages/Service";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       {/* BrowserRouter takes the default path of the browser */}
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <div>LearnWeb</div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Services</li>
+            <li>Login</li>
+            <li>Signup</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
