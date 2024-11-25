5938efb6026c36bd962bdda86134f6cf50875c08
SinBin10 <bincode10@gmail.com>
2024-11-26 00:53:41 +0530

    making the registeration page.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,79 @@
+import { useState } from "react";
+
 const Register = () => {
-  return <h1 className="text-white">This is the Register Page.</h1>;
+  const [input, setInput] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+  const handleInput = (e) => {
+    console.log(e);
+    let name = e.target.name;
+    setInput({
+      ...input,
+      [name]: e.target.value,
+    });
+  };
+  return (
+    <div className="flex text-white mx-52 gap-40">
+      <div className="w-[46%]">
+        <img
+          src="../../public/images/register.png"
+          alt="a girl using a phone"
+        />
+      </div>
+      <div>
+        <h1 className="text-5xl font-bold my-4">Registration Form</h1>
+        <form action="submit" className="flex flex-col gap-6">
+          <div>
+            <label htmlFor="username">Username: </label>
+            <input
+              type="text"
+              className="text-black"
+              name="username"
+              value={input.username}
+              onChange={handleInput}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              className="text-black"
+              name="email"
+              value={input.email}
+              onChange={handleInput}
+            />
+          </div>
+          <div>
+            <label htmlFor="phone">Phone: </label>
+            <input
+              type="text"
+              className="text-black"
+              name="phone"
+              value={input.phone}
+              onChange={handleInput}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              className="text-black"
+              name="password"
+              value={input.password}
+              onChange={handleInput}
+            />
+          </div>
+          <input
+            className="bg-white text-black cursor-pointer w-[20%] rounded-lg"
+            type="submit"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
