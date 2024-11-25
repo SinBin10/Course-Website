3172908f5289c0912527675e0b3b291132079a42
SinBin10 <bincode10@gmail.com>
2024-11-26 00:52:04 +0530

    adding images and making some changes in navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,31 +4,33 @@ const Navbar = () => {
   return (
     <>
       <header className="text-purple-600">
-        <div className="logo">
-          <NavLink to="/">LearnWeb</NavLink>
+        <div className="flex justify-between max-w-[76rem] px-16 py-9 m-auto">
+          <div className="logo">
+            <NavLink to="/">LearnWeb</NavLink>
+          </div>
+          <nav>
+            <ul className="flex gap-8">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+              <li>
+                <NavLink to="/service">Services</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+              <li>
+                <NavLink to="/register">Register</NavLink>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact">Contact</NavLink>
-            </li>
-            <li>
-              <NavLink to="/service">Services</NavLink>
-            </li>
-            <li>
-              <NavLink to="/login">Login</NavLink>
-            </li>
-            <li>
-              <NavLink to="/register">Register</NavLink>
-            </li>
-          </ul>
-        </nav>
       </header>
     </>
   );
