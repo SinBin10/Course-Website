c3b75f889f79e3189ca8b069cf8c64f5d1142b69
SinBin10 <bincode10@gmail.com>
2024-12-10 11:43:13 +0530

    styling the buttons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,41 @@
 const Home = () => {
-  return <h1 className="text-white">This is the Home Page.</h1>;
+  return (
+    <main>
+      <section className="text-white">
+        <div className="flex mx-52 gap-40 items-center">
+          <div className="max-w-[31rem]">
+            <p className=" text-gray-200">We are the World Best IT Company</p>
+            <h1 className="text-5xl font-bold my-4">
+              Welcome to Learn <br /> Web
+            </h1>
+            <p className=" text-gray-200">
+              Are you ready to take your business to the next level with
+              cutting-edge IT solutions ? Look no further! At LearnWeb, we
+              specialize in providing innovative IT services and solutions
+              tailored to meet your unique needs.
+            </p>
+            <div className="my-10 text-sm flex items-center gap-8">
+              <a className="bg-purple-600 px-6 py-2 rounded-lg" href="/contact">
+                Connect Now
+              </a>
+              <a
+                className="px-6 py-2 rounded-lg border-[1px] border-gray-500"
+                href="/service"
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+          <div className="w-[46%]">
+            <img
+              src="../../public/images/home.png"
+              alt="image of a workplace"
+            />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
